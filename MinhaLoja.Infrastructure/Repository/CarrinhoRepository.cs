466c4deaa74850a8fc6a9d86df0d282f4188032a
KaylanDev <k1ndealblack@gmail.com>
2025-09-24 22:58:29 -0300

    feat: criado metodo para pegar carrinho via usarId

--- MinhaLoja.Infrastructure/Repository/CarrinhoRepository.cs
+++ MinhaLoja.Infrastructure/Repository/CarrinhoRepository.cs
@@ -1,6 +1,8 @@
-﻿using MinhaLoja.Domain.Interfaces;
+﻿using Microsoft.EntityFrameworkCore;
+using MinhaLoja.Domain.Interfaces;
 using MinhaLoja.Domain.Models;
 using MinhaLoja.Infrastructure.Context;
+using MongoDB.Bson;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +15,13 @@ namespace MinhaLoja.Infrastructure.Repository
     {
         public CarrinhoRepository(AppDbContext context) : base(context)
         {
+        }
+
+        public async Task<Carrinho> GetByUserIdAsync(ObjectId usuarioId)
+        {
+            var carrinho = await _context.Carrinho.FirstOrDefaultAsync(c => c.UsuarioId == usuarioId);
+            if (carrinho is null) return null;
+            return carrinho;
         }
     }
 }
