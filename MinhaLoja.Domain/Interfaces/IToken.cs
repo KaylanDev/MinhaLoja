32e1cc1c8e5026e360e99c61619819e2442bbfa1
KaylanDev <k1ndealblack@gmail.com>
2025-10-07 23:58:17 -0300

    feat: criado Token e IToken, falta implementar metodos

---
+++ MinhaLoja.Domain/Interfaces/IToken.cs
@@ -0,0 +1,16 @@
+﻿using MinhaLoja.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinhaLoja.Domain.Interfaces
+{
+    public interface IToken
+    {
+        public string GenerateToken(Usuario user,List<string> claims);
+        public string GenerateRefreshToken();
+
+    }
+}
---
+++ MinhaLoja.Infrastructure/TokenGenerate/Token.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using MinhaLoja.Domain.Interfaces;
+using MinhaLoja.Domain.Models;
+using MinhaLoja.Infrastructure.ContextPgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinhaLoja.Infrastructure.TokenGenerate;
+
+public class Token : IToken
+{
+    private readonly IConfiguration _configuration;
+    private readonly UserManager<UserIdentity> _userManager;
+
+    public Token(IConfiguration configuration, UserManager<UserIdentity> userManager)
+    {
+        _configuration = configuration;
+        _userManager = userManager;
+    }
+
+    public string GenerateRefreshToken()
+    {
+        throw new NotImplementedException();
+    }
+
+    public string GenerateToken(Usuario user, List<string> claims)
+    {
+        throw new NotImplementedException();
+    }
+}
